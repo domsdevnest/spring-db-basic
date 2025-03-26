5e825c8d4f798669a544423f04a8a0d89449f703
domsdevnest <domsdevnest@gmail.com>
2025-03-26 22:00:49 +0900

    데이터 조회

--- jdbc/src/test/java/com/domsdevnest/jdbc/repository/MemberRepositoryTest.java
+++ jdbc/src/test/java/com/domsdevnest/jdbc/repository/MemberRepositoryTest.java
@@ -1,10 +1,15 @@
 package com.domsdevnest.jdbc.repository;
 
 import com.domsdevnest.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
 public class MemberRepositoryTest {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
@@ -13,5 +18,10 @@ public class MemberRepositoryTest {
     void crud() throws SQLException {
         Member member = new Member("memberV0", 10000);
         repository.save(member);
+
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
+
 }
