d653e25fc44e83b16577209ca0664e4f153eba2a
domsdevnest <domsdevnest@gmail.com>
2025-03-26 22:16:19 +0900

    데이터 수정/삭제

--- jdbc/src/test/java/com/domsdevnest/jdbc/repository/MemberRepositoryTest.java
+++ jdbc/src/test/java/com/domsdevnest/jdbc/repository/MemberRepositoryTest.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -22,6 +23,15 @@ public class MemberRepositoryTest {
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember={}", findMember);
         assertThat(findMember).isEqualTo(member);
+
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        log.info("updateMember={}", updateMember);
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        repository.delete(member.getMemberId());
+        Assertions.assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 
 }
