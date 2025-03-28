251ad5018aef57ddf43267b332da6c777d0923d4
domsdevnest <domsdevnest@gmail.com>
2025-03-28 22:16:01 +0900

    커넥션 풀을 이용한 커넥션 획득 예제

--- jdbc/src/test/java/com/domsdevnest/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/com/domsdevnest/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package com.domsdevnest.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -33,5 +34,18 @@ public class ConnectionTest {
         Connection con2 = dataSource.getConnection();
         log.info("connection={}, class={}", con1,con1.getClass());
         log.info("connection={}, class={}", con2,con2.getClass());
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
     }
 }
