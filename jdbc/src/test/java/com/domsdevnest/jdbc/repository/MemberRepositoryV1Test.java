885d59efe7f4df4021377cc1c27c810ed5cb17ad
domsdevnest <domsdevnest@gmail.com>
2025-03-29 14:24:24 +0900

    DataSource 의 적용

---
+++ jdbc/src/test/java/com/domsdevnest/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,60 @@
+package com.domsdevnest.jdbc.repository;
+
+import com.domsdevnest.jdbc.domain.Member;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static com.domsdevnest.jdbc.connection.ConnectionConst.*;
+@Slf4j
+public class MemberRepositoryV1Test {
+
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach() {
+
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL,USERNAME,PASSWORD);
+//        repository = new MemberRepositoryV1(dataSource);
+
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        repository = new MemberRepositoryV1(dataSource);
+    }
+
+    @Test
+    void crud() throws SQLException {
+        Member member = new Member("memberV0", 10000);
+        repository.save(member);
+
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
+
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        log.info("updateMember={}", updateMember);
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        repository.delete(member.getMemberId());
+        Assertions.assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
